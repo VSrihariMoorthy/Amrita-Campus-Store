90802ff1de17cff9cad04db25425615b6bc102c6
VSrihariMoorthy <sriharimoorthy02@gmail.com>
2023-10-20 10:17:32 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:project/screens/landingPage.dart';
+import "package:project/firebase_options.dart";
+import 'package:provider/provider.dart';
+import 'notifiers/authNotifier.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/services.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(
+        create: (_) => AuthNotifier(),
+      ),
+      // ChangeNotifierProvider(
+      //   create: (_) => FoodNotifier(),
+      // ),
+    ],
+    child: MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'SAACS',
+      theme: ThemeData(
+        fontFamily: 'Montserrat',
+        primaryColor: const Color.fromRGBO(255, 63, 111, 1),
+      ),
+      home: Scaffold(
+        body: LandingPage(),
+      ),
+    );
+  }
+}
---
+++ lib/notifiers/authNotifier.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/foundation.dart';
+import 'package:firebase_auth/firebase_auth.dart' as auth;
+import 'package:project/models/user.dart';
+
+class AuthNotifier extends ChangeNotifier {
+
+  auth.User? _user;
+
+  auth.User? get user {
+    return _user;
+  }
+
+  void setUser(auth.User? user) {
+    _user = user;
+    notifyListeners();
+  }
+
+  // Test
+  late User _userDetails;
+
+  User? get userDetails => _userDetails;
+
+  setUserDetails(User user) {
+    _userDetails = user;
+    notifyListeners();
+  }
+}
